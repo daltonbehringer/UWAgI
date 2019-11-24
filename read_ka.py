9155766d515622e0191e9ab0f50b2efc9c563909
Dalton Behringer <jbehrin1@uwyo.edu>
2019-11-24 12:49:25 -0700

    Change ncvars to include attrs

--- read_ka.py
+++ read_ka.py
@@ -16,36 +16,36 @@ class _reader(object):
 
         nc = Dataset(filename)
 
-        self.u = nc.variables['avux'][:]
-        self.v = nc.variables['avvy'][:]
-        self.ka_lat = nc.variables['GLAT'][:]
-        self.ka_lon = nc.variables['GLON'][:]
-        self.ka_time = nc.variables['TIME'][:]
-        self.ka_alt = nc.variables['avalt'][:]
-        self.wind_mag = nc.variables['avwmag'][:]
-        self.wind_dir = nc.variables['avwdir'][:]
-        self.roll = nc.variables['avroll'][:]
+        self.u = nc.variables['avux']
+        self.v = nc.variables['avvy']
+        self.ka_lat = nc.variables['GLAT']
+        self.ka_lon = nc.variables['GLON']
+        self.ka_time = nc.variables['TIME']
+        self.ka_alt = nc.variables['avalt']
+        self.wind_mag = nc.variables['avwmag']
+        self.wind_dir = nc.variables['avwdir']
+        self.roll = nc.variables['avroll']
 
         # self._read_file()
-#         self._prep_data()
+        self._prep_data()
 
-#     def _prep_data(self):
+    def _prep_data(self):
 
-#         self.fields["u_wind"] = ncvar_to_dict(self.u)
-#         self.fields["v_wind"] = ncvar_to_dict(self.v)
-#         self.fields["ka_lat"] = ncvar_to_dict(self.ka_lat)
-#         self.fields["ka_lon"] = ncvar_to_dict(self.ka_lon)
-#         self.fields["ka_time"] = ncvar_to_dict(self.ka_time)
-#         self.fields["ka_alt"] = ncvar_to_dict(self.ka_alt)
-#         self.fields["wind_mag"] = ncvar_to_dict(self.wind_mag)
-#         self.fields["wind_dir"] = ncvar_to_dict(self.wind_dir)
-#         self.fields["roll"] = ncvar_to_dict(self.roll)
+        self.fields["u_wind"] = ncvar_to_dict(self.u)
+        self.fields["v_wind"] = ncvar_to_dict(self.v)
+        self.fields["ka_lat"] = ncvar_to_dict(self.ka_lat)
+        self.fields["ka_lon"] = ncvar_to_dict(self.ka_lon)
+        self.fields["ka_time"] = ncvar_to_dict(self.ka_time)
+        self.fields["ka_alt"] = ncvar_to_dict(self.ka_alt)
+        self.fields["wind_mag"] = ncvar_to_dict(self.wind_mag)
+        self.fields["wind_dir"] = ncvar_to_dict(self.wind_dir)
+        self.fields["roll"] = ncvar_to_dict(self.roll)
 
-# def ncvar_to_dict(ncvar):
+def ncvar_to_dict(ncvar):
     
-#     d = dict((k, getattr(ncvar, k)) for k in ncvar.ncattrs())
-#     d["data"] = ncvar[:]
-#     if np.isscalar(d["data"]):
-#         d["data"] = np.array(d["data"][:])
-#         d["data"].shape = (1,)
-#     return d
+    d = dict((k, getattr(ncvar, k)) for k in ncvar.ncattrs())
+    d["data"] = ncvar
+    if np.isscalar(d["data"]):
+        d["data"] = np.array(d["data"])
+        d["data"].shape = (1,)
+    return d
