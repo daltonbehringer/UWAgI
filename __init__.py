e44aa79a12088f4c5caf8b993ed521ea77dbb7bc
Dalton Behringer <jbehrin1@uwyo.edu>
2019-11-24 13:23:25 -0700

    Move inits

--- __init__.py
+++
@@ -1,2 +0,0 @@
-from .readers.read_ka import KAReader
-from .readers.read_seeder import SeederReader
--- uwagi/__init__.py
+++ uwagi/__init__.py
@@ -1,3 +1,6 @@
 #initialization file
 #Dalton Behringer
-#University of Wyoming, Dept. of Atmspheric Science
+#University of Wyoming, Dept. of Atmospheric Science
+
+from .readers.read_ka import KAReader
+from .readers.read_seeder import SeederReader
