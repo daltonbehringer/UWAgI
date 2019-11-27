ace2f4dc8d25d83eae5e38a546ca420196acd41a
Dalton Behringer <jbehrin1@uwyo.edu>
2019-11-27 11:56:31 -0700

    initial plot file

---
+++ uwagi/plot/plot.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+from matplotlib.dates import DateFormatter
+from matplotlib.dates import SecondLocator, MinuteLocator, HourLocator, DayLocator
+
