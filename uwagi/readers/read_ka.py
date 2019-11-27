1cb9f19c91e905bb46e651158ead16b07216794c
Dalton Behringer <jbehrin1@uwyo.edu>
2019-11-27 10:56:08 -0700

    added variables

--- uwagi/readers/read_ka.py
+++ uwagi/readers/read_ka.py
@@ -35,6 +35,15 @@ class _reader(object):
         self.wind_mag = nc.variables['avwmag']
         self.wind_dir = nc.variables['avwdir']
         self.roll = nc.variables['avroll']
+        self.lwc100 = nc.variables['lwc100']
+        self.nevlwc = nc.variables['nevlwc']
+        self.nevtwc = nc.variables['nevtwc']
+        self.cdplwc = nc.variables['cdplwc_NRB']
+        self.cdpconc = nc.variables['cdpconc_NRB']
+        self.pvmlwc = nc.variables['pvmlwc']
+        self.temp = nc.variables['trf']
+        self.dwpt = nc.variables['tdp70']
+        self.airspeed = nc.variables['tas']
 
         self._prep_data()
 
@@ -51,6 +60,15 @@ class _reader(object):
         self.fields["wind_mag"] = ncvar_to_dict(self.wind_mag)
         self.fields["wind_dir"] = ncvar_to_dict(self.wind_dir)
         self.fields["roll"] = ncvar_to_dict(self.roll)
+        self.fields["lwc100"] = ncvar_to_dict(self.lwc100)
+        self.fields["nev_lwc"] = ncvar_to_dict(self.nevlwc)
+        self.fields["nev_twc"] = ncvar_to_dict(self.nevtwc)
+        self.fields["cdp_lwc"] = ncvar_to_dict(self.cdplwc_NRB)
+        self.fields["cdp_conc"] = ncvar_to_dict(self.cdpconc_NRB)
+        self.fields["lwc_pvm"] = ncvar_to_dict(self.pvmlwc)
+        self.fields["temperature"] = ncvar_to_dict(self.trf)
+        self.fields["dewpoint"] = ncvar_to_dict(self.tdp70)
+        self.fields["airspeed"] = ncvar_to_dict(self.tas)
 
 def ncvar_to_dict(ncvar):
     
