9e3a561e06c6ad91732319e66587d69cee0f48f5
Dalton Behringer <jbehrin1@uwyo.edu>
2019-11-27 12:17:51 -0700

    data_corr update

--- uwagi/plot/plot.py
+++ uwagi/plot/plot.py
@@ -5,3 +5,9 @@ import matplotlib as mpl
 from matplotlib.dates import DateFormatter
 from matplotlib.dates import SecondLocator, MinuteLocator, HourLocator, DayLocator
 
+from .utility import data_corr
+
+'''Lat/Lon for Packer John radar (SNOWIE 2017). Update as needed.'''
+pj_lat = 44.207692
+pj_lon = -116.0693
+
--- uwagi/utility/data_corr.py
+++ uwagi/utility/data_corr.py
@@ -1,2 +1,4 @@
 '''Includes data corrections for microphysical data from SNOWIE 2017'''
 
+### NEED TO UPDATE THIS FOR DATA CORRECTIONS ###
+
