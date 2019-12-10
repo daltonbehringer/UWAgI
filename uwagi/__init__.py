5b414191accaebec768c12d3b9cf46d59e272a59
Dalton Behringer <jbehrin1@uwyo.edu>
2019-12-10 14:46:31 -0700

    update init

--- uwagi/__init__.py
+++ uwagi/__init__.py
@@ -4,6 +4,7 @@
 
 from .readers.read_ka import read_ka
 from .readers.read_seeder import read_seeder
+from .readers.read_sizedist import read_sizedist
 
 from .utility import data_corr
 from .plot import plot
