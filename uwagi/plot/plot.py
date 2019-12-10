3a96fda7277dbcfb65a19950ee6e247638c36618
Dalton Behringer <jbehrin1@uwyo.edu>
2019-12-10 15:11:19 -0700

    plot_dist

--- uwagi/plot/plot.py
+++ uwagi/plot/plot.py
@@ -9,38 +9,44 @@ from matplotlib.dates import SecondLocator, MinuteLocator, HourLocator, DayLocat
 clat = 44.207692
 clon = -116.0693
 
-def plot(
-    ka,
-    var,
-    clat,
-    clon,
-    xlims = None,
-    ylims = None,
-    tighten = True,
-    vmin = None,
-    vmax = None,
-    cmap = None,
-    ax = None,
-    fig = None
-    ):
-
-    ax = parse_ax(ax)
-    fig = parse_fig(fig)
+def plot_dist(
     
-    if vmin is None:
-        vmin = np.nanmin(ka.fields[str(var)]["data"])
-    if vmax is None:
-        vmax = np.nanmax(ka.fields[str(var)]["data"])
+    )
 
-    ### MAYBE MOVE THIS TO SEPARATE FUNC ###
-    # kdist = np.zeros((len(ka.time[:])))
-    # for i in range(len(kdist)-1):
-    #     kdist[i] = 
+# def plot(
+#     ka,
+#     var,
+#     clat,
+#     clon,
+#     xlims = None,
+#     ylims = None,
+#     tighten = True,
+#     vmin = None,
+#     vmax = None,
+#     cmap = None,
+#     ax = None,
+#     fig = None
+#     ):
 
-    plt.plot(
-        ka.time[:].filled(),
-        ka.str(var)[:].filled(),
-        vmin = vmin,
-        vmax = vmax)
+#     ax = parse_ax(ax)
+#     fig = parse_fig(fig)
+    
+#     if vmin is None:
+#         vmin = np.nanmin(ka.fields[str(var)]["data"])
+#     if vmax is None:
+#         vmax = np.nanmax(ka.fields[str(var)]["data"])
+
+#     ### MAYBE MOVE THIS TO SEPARATE FUNC ###
+#     # kdist = np.zeros((len(ka.time[:])))
+#     # for i in range(len(kdist)-1):
+#     #     kdist[i] = 
+
+#     plt.plot(
+#         ka.time[:].filled(),
+#         ka.str(var)[:].filled(),
+#         vmin = vmin,
+#         vmax = vmax)
+
+
 
 
