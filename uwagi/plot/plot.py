09f344d5fcef36d4841db1746a39fc15a5c100d5
Dalton Behringer <jbehrin1@uwyo.edu>
2020-04-13 13:48:39 -0600

    sd fix

--- uwagi/plot/plot.py
+++ uwagi/plot/plot.py
@@ -124,13 +124,13 @@ def plot_sd(
     '''
     Plots a size distribution
 
+    If not start time, entire leg is averaged
+
     Usage: uwagi.plot.plot_sd(args)
 
     Arguments:
         ka = data object
-        var = which probe to get size dist from, or average ('mean')
         iop = iop number
-        leg = leg of the iop to plot
 
     Returns: matplotlib plot object
     '''
@@ -166,8 +166,6 @@ def plot_sd(
 
     ax.plot(bins, sd, ls=ls, c=c, marker=marker, **kwargs)
 
-    # ax.yaxis.set_minor_locator(ticker.AutoMinorLocator())
-
     ax.set_xscale('log')
     ax.set_xlim([1E1, 1E4])
     ax.set_yscale('log')
@@ -184,105 +182,6 @@ def plot_sd(
     #     ax.set_title(title, fontdict=font)
 
     return fig, ax
-
-'''Plot size distribution time-series'''
-
-# def plot_sd_hov(
-#     ka,
-#     var,
-#     iop = None,
-#     leg = None,
-#     title = None,
-#     y_label = None,
-#     ax = None,
-#     fig = None,
-#     tighten = True,
-#     ls = None,
-#     c = None,
-#     marker = None,
-#     **kwargs
-#     ):
-
-    '''
-    Plots a size distribution using a Hovmoller-type plot
-
-    Usage: uwagi.plot.plot_sd_hov(args)
-
-    Arguments:
-        ka = data object
-        var = which probe to get size dist from, or average ('mean')
-        iop = iop number
-        leg = leg of the iop to plot
-
-    Returns: matplotlib plot object
-    '''
-
-    # fig = parse_fig(fig,10,4)
-    # ax = parse_ax(ax)
-
-    # # start_time, end_time = get_times(iop, leg)[0], get_times(iop, leg)[1]
-
-    # # start = np.where(ka.fields['time'] == np.datetime64(start_time))[0][0]
-    # # end = np.where(ka.fields['time'] == np.datetime64(end_time))[0][0]
-
-    # time = ka.time
-    # ind = np.array(((np.where(time == 165551)[0][0]),(np.where(time == 170749)[0][0])))
-
-    # # if var is '2ds' or '2DS':
-    # sd1 = ka.dist_2DS_H[ind[0]:ind[1],:]# * ka.dist_2DS_V) / 2
-    # bins1 = ka.mid_2DS
-    # # sd1[sd1 == 0.] = np.nan
-    # # if var is '2dp' or '2DP':
-    # dist2 = ka.dist_2DP[ind[0]:ind[1],:]
-    # bins2 = ka.mid_2DP
-    # # dist2[dist2 == 0.] = np.nan
-    # #     sd = np.nanmean(dist, axis=0)
-    # # if var is 'cdp' or 'CDP':
-    # #     dist = ka.dist_CDP
-    # #     bins = ka.mid_CDP
-    # #     dist[dist == 0.] = np.nan
-    # #     sd = np.nanmean(dist, axis=0)
-    # # if var is 'cip' or 'CIP':
-    # #     dist = ka.dist_CIP
-    # #     bins = ka.mid_CIP
-    # #     dist[dist == 0.] = np.nan
-    # #     sd = np.nanmean(dist, axis=0)
-
-    # y1, x1 = np.meshgrid(bins1, time[ind[0]:ind[1]])
-    # y2, x2 = np.meshgrid(bins2, time[ind[0]:ind[1]])
-    
-    # cmap = plt.get_cmap('plasma')
-
-    # if ls is None:
-    #     ls = '-'
-    # if c is None:
-    #     c = 'k'
-
-    # ax.set_yscale('log')
-    # ax.set_ylim([1E1, 1E4])
-
-    # ax.grid(True)
-    # ax.tick_params(axis='both', which='both', direction='in', length = 7, grid_linestyle='--', grid_alpha=0.5)
-    
-    # ax.set_xlabel(r'Time, UTC')
-    # ax.set_ylabel(r'Particle Diameter, $\mu m$')
-
-    # vmin, vmax = 1E-8, 1E0
-
-    # sd_plot = plt.pcolormesh(x1, y1, sd1, 
-    #     norm=colors.LogNorm(vmin=vmin,vmax=vmax), cmap=cmap)
-    # sd_plot2 = plt.pcolormesh(x2, y2, dist2, 
-    #     norm=colors.LogNorm(vmin=vmin,vmax=vmax), cmap=cmap)
-
-    # fig.colorbar(sd_plot, ax=ax, label=r'# $cm^{-3}\/\mu m^{-1}$')
-
-    # # if title is None:
-    # #     ax.set_title(start_time[0:10]+' | IOP '+str(iop)+' | Leg '+str(leg), fontdict=font)
-    # # elif title is not None:
-    # #     ax.set_title(title, fontdict=font)
-
-    # return fig, ax
-
 
 def parse_fig(fig,x,y):
     """ Parse and return fig instance. """
