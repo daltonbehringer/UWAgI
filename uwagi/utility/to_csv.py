fdc0680754b5ee20809b81132b8a85c2ec9d23a4
Dalton Behringer <jbehrin1@uwyo.edu>
2020-04-13 15:37:32 -0600

    csv fix

--- uwagi/utility/to_csv.py
+++ uwagi/utility/to_csv.py
@@ -14,30 +14,39 @@ def sd_to_csv(
     outdir = None
     ):
 
-if start_time is None:
-    start_time = int(sd.time[0])
-if end_time is None:
-    end_time = int(sd.time[-1])
+    if start_time is None:
+        start_time = int(sd.time[0])
+    if end_time is None:
+        end_time = int(sd.time[-1])
 
-sd_CDP, sd_2DS, sd_2DP, bins_CDP, bins_2DS, bins_2DP = sd_corr(sd, start_time, end_time)
+    sd_CDP, sd_2DS, sd_2DP, bins_CDP, bins_2DS, bins_2DP = sd_corr(sd, start_time, end_time)
 
-# df = pd.DataFrame(data = sd.time.astype(int), index = None, columns = ['HHMMSS'])
-df = pd.DataFrame(data = None, index = None, columns = None)
+    bin_min = np.append(bins_CDP[0], bins_2DS[0])
+    bin_min = np.append(bin_min, bins_2DP[0])
+    
+    bin_mid = np.append(bins_CDP[1], bins_2DS[1])
+    bin_mid = np.append(bin_mid, bins_2DP[1])
+    
+    bin_max = np.append(bins_CDP[2], bins_2DS[2])
+    bin_max = np.append(bin_max, bins_2DP[2])
 
-df['bin_min_CDP'], df['bin_mid_CDP'], df['bin_max_CDP'] = bins_CDP[0], bins_CDP[1], bins_CDP[2]
-df['sd_CDP'] = sd_CDP
+    sd = np.append(sd_CDP, sd_2DS)
+    sd = np.append(sd, sd_2DP)
 
-df['bin_min_2DS'], df['bin_mid_2DS'], df['bin_max_2DS'] = bins_2DS[0], bins_2DS[1], bins_2DS[2]
-df['sd_2DS'] = sd_2DS
+    # df = pd.DataFrame(data = sd.time.astype(int), index = None, columns = ['HHMMSS'])
+    df = pd.DataFrame(data = None, index = None, columns = None)
 
-df['bin_min_2DP'], df['bin_mid_2DP'], df['bin_max_2DP'] = bins_2DP[0], bins_2DP[1], bins_2DP[2]
-df['sd_2DP'] = sd_2DP
+    df['bin_min'] = bin_min
+    df['bin_mid'] = bin_mid
+    df['bin_max'] = bin_max
+    
+    df['size_dist'] = sd
 
-if outdir is None:
-    filename = 'sizedist_'+str(start_time)+'_'+str(end_time)+'.csv'
-if outdir is not None:
-    filename = outdir+'sizedist_'+str(start_time)+'_'+str(end_time)+'.csv'
+    if outdir is None:
+        filename = 'sizedist_'+str(start_time)+'_'+str(end_time)+'.csv'
+    if outdir is not None:
+        filename = outdir+'sizedist_'+str(start_time)+'_'+str(end_time)+'.csv'
 
-csv_out = df.to_csv(filename, sep = ',', index = False, enconding = 'utf-8')
+    csv_out = df.to_csv(filename, sep = ',', index = False, enconding = 'utf-8')
 
-return csv_out
+    return csv_out
