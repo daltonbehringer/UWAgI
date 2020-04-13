dea8fed10967ab45486182c8be269c68565a7c2c
Dalton Behringer <jbehrin1@uwyo.edu>
2020-04-13 14:01:56 -0600

    2DP bin fix

--- uwagi/utility/data_corr.py
+++ uwagi/utility/data_corr.py
@@ -8,18 +8,18 @@ Adjust size distribution to set new bins
 
 def sd_corr(sd, start_time, end_time):
 
-    bins_2DP = np.arange(100,20300,200)
-    bins1 = np.arange(500,2100,200)
-    bins2 = np.arange(2100,21100,1000)
-    bins_2DP_new = np.append(bins1,bins2)
-    bin_mid_2DP = (bins_2DP[1:] + bins_2DP[0:-1]) / 2
-    bin_min_2DP_new = bins_2DP_new[0:-1]
-    bin_max_2DP_new = bins_2DP_new[1:]
-    bin_dD_2DP = bins_2DP[1:] - bins_2DP[0:-1]
+    # bins_2DP = np.arange(100,20300,200)
+    # bins1 = np.arange(500,2100,200)
+    # bins2 = np.arange(2100,21100,1000)
+    # bins_2DP_new = np.append(bins1,bins2)
+    # bin_mid_2DP = (bins_2DP[1:] + bins_2DP[0:-1]) / 2
+    # bin_min_2DP_new = bins_2DP_new[0:-1]
+    # bin_max_2DP_new = bins_2DP_new[1:]
+    # bin_dD_2DP = bins_2DP[1:] - bins_2DP[0:-1]
 
-    bin_mid_2DP_new = (bins_2DP_new[1:] + bins_2DP_new[0:-1]) / 2
-    bin_dD_2DP_new = bins_2DP_new[1:] - bins_2DP_new[0:-1]
-    sd_2DP_new = np.zeros_like(bin_dD_2DP_new)
+    # bin_mid_2DP_new = (bins_2DP_new[1:] + bins_2DP_new[0:-1]) / 2
+    # bin_dD_2DP_new = bins_2DP_new[1:] - bins_2DP_new[0:-1]
+    # sd_2DP_new = np.zeros_like(bin_dD_2DP_new)
 
     bin_mid_CDP = sd.bin_mid_CDP.data
     bin_min_CDP = sd.bin_min_CDP.data
@@ -55,9 +55,12 @@ def sd_corr(sd, start_time, end_time):
         ind_i = np.where(np.logical_and(sd.bin_mid_2DS >= bin_min_2DS_new[i], sd.bin_mid_2DS <= bin_max_2DS_new[i]))
         sd_2DS_new[i] = np.nansum(sd_2DS[ind_i] * sd.bin_dD_2DS.data[ind_i]) / np.nansum(sd.bin_dD_2DS.data[ind_i])
 
-    for j in range(0, len(bin_dD_2DP_new)):
-        ind_j = np.where(np.logical_and(sd.bin_mid_2DP >= bin_min_2DP_new[j], sd.bin_mid_2DP <= bin_max_2DP_new[j]))
-        sd_2DP_new[j] = np.nansum(sd_2DP[ind_j] * sd.bin_dD_2DP.data[ind_j]) / np.nansum(sd.bin_dD_2DP.data[ind_j])
+    # for j in range(0, len(bin_dD_2DP_new)):
+    #     ind_j = np.where(np.logical_and(sd.bin_mid_2DP >= bin_min_2DP_new[j], sd.bin_mid_2DP <= bin_max_2DP_new[j]))
+    #     sd_2DP_new[j] = np.nansum(sd_2DP[ind_j] * sd.bin_dD_2DP.data[ind_j]) / np.nansum(sd.bin_dD_2DP.data[ind_j])
+    sd_2DP_new = sd_2DP
+    bin_mid_2DP_new = sd.bin_mid_2DP
+
 
     return sd_CDP, sd_2DS_new, sd_2DP_new, bin_mid_CDP, bin_mid_2DS_new, bin_mid_2DP_new
 
